8ae8a874c69b9aecf603474a3e2a293fb07c54d3
edurbrito <developer.edurbrito@gmail.com>
2022-03-08 17:54:32 +0200

    add tailwind.css support ; add repo structure

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<span class="p-10 flex flex-col">
+	<h1 class="font-bold underline">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</span>
