dd3fbe2c3c6623f9aca1d96b9c5ae99ece200cd4
edurbrito <developer.edurbrito@gmail.com>
2022-04-13 23:24:43 +0300

    fix hider view

--- web/src/routes/hide/[gameid].svelte
+++ web/src/routes/hide/[gameid].svelte
@@ -1,18 +1,96 @@
 <script>
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
 
-    let gameid = ''
-    let distance = '0'
+	const game_id = $page.params.gameid;
+	let hider;
+	let username = '';
+	let distance = 'some';
+	let found = false;
+	var currentPosition = {
+		latitude: 0,
+		longitude: 0
+	};
 
-    onMount(() => {
-        gameid = localStorage.getItem('gameid')
-        distance = localStorage.getItem('distance')
-    })
+	const options = {
+		enableHighAccuracy: true,
+		maximumAge: 60000,
+		timeout: 30000
+	};
+
+	onMount(async () => {
+		hider = JSON.parse(localStorage.getItem('hider'));
+		if (hider == null || !hider) {
+			await fetch(`http://localhost:5000/hide/${game_id}`, { method: 'POST' })
+				.then((response) => response.json())
+				.then((response) => {
+					localStorage.setItem('hider', JSON.stringify(response));
+					hider = response;
+				});
+		}
+
+		username = hider.username;
+
+		if ('geolocation' in navigator) {
+			getCurrentPosition();
+			setInterval(getCurrentPosition, 15000);
+		} else {
+			document.getElementById('warning').style.setProperty('display', 'flex');
+		}
+
+		function getCurrentPosition() {
+			navigator.geolocation.getCurrentPosition(
+				async (position) => {
+					currentPosition = {
+						latitude: position.coords.latitude,
+						longitude: position.coords.longitude
+					};
+
+					await fetch(
+						`http://localhost:5000/hider/${game_id}?uuid=${hider.uuid}&latitude=${currentPosition.latitude}&longitude=${currentPosition.longitude}`,
+						{ method: 'POST' }
+					)
+						.then((response) => response.json())
+						.then((response) => {
+							found = response.found;
+							distance = response.distance;
+						});
+				},
+				function (error) {
+					switch (error.code) {
+						case error.PERMISSION_DENIED:
+							console.log('Denied request for Geolocation.');
+							break;
+						case error.POSITION_UNAVAILABLE:
+							console.log('Location unavailable.');
+							break;
+						case error.TIMEOUT:
+							console.log('Location request timed out.');
+							break;
+						default:
+							console.log('Default case.');
+							break;
+					}
+					document.getElementById('warning').style.setProperty('display', 'flex');
+				},
+				options
+			);
+		}
+	});
 </script>
 
-<span class="mb-5 font-semibold">Username</span>
+<span class="mb-5 font-semibold">{username}</span>
 <div class="game-circle">
-    <span class="text-lg">Game ID</span>
-    <span class="text-2xl font-bold">{gameid}</span>
+	<span class="text-lg">Game ID</span>
+	<span class="text-2xl font-bold">{game_id.toUpperCase()}</span>
 </div>
-<span class="mt-5 text-center text-lg">The Seeker is {Math.round(Number(distance))} meters from you!</span>
+<span class="mt-5 text-center text-lg"
+	>The Seeker is <br />{distance == 'some' ? distance : Math.round(parseFloat(distance))} meters from
+	you!</span
+>
+{#if found}
+	<div class="text-white text-2xl font-bold text-center">
+		You were found!<br />🙈🙉<br />
+		<span class="text-base font-normal">Better luck next time...</span>
+	</div>
+{/if}
--- web/src/routes/hide/index.svelte
+++ web/src/routes/hide/index.svelte
@@ -1,31 +1,34 @@
 <script>
-    let userData = {}
-    let gameid = ''
+	let gameid = '';
 
-    const handleSubmitButtonClick = async (event) => {
-        event.preventDefault()
-        await fetch(`http://localhost:5000/hide/${gameid}`, {method: "POST"})
-            .then((response) => response.json())
-            .then(obj => {
-                userData = obj 
-            })
-
-        await fetch(`http://localhost:5000/hider/${gameid}?uuid=${userData.uuid}&latitude=30.001&longitude=20.001`, {method: "POST"})
-            .then((response) => response.json())
-            .then(obj => localStorage.setItem('distance', obj.distance))
-        
-        localStorage.setItem('gameid', gameid)
-
-        window.location.href = `/hide/${gameid}?uuid=${userData.uuid}&latitude=30.001&longitude=20.001`
-    }
+	const handleSubmitButtonClick = async (event) => {
+		event.preventDefault();
+		await fetch(`http://localhost:5000/hide/${gameid}`, { method: 'POST' })
+			.then((response) => response.json())
+			.then((hider) => {
+				localStorage.setItem('hider', JSON.stringify(hider));
+				window.location.href = `/hide/${gameid}`;
+			});
+	};
 </script>
 
 <div>
 	<h1 class="title">Hide & Seek</h1>
-    <form class="mt-3">
-        <input bind:value={gameid} class="input bg-other other-text focus:outline-none focus:ring-1 focus:ring-slate-600" type="text" maxlength="6" size="1">
-        <button on:click={handleSubmitButtonClick} class="button bg-secondary other-text" type="sumbit" disabled={!(gameid.length === 6)}>
-            Join Game
-        </button>
-    </form>
+	<form class="mt-3">
+		<input
+			bind:value={gameid}
+			class="input bg-other other-text focus:outline-none focus:ring-1 focus:ring-slate-600"
+			type="text"
+			maxlength="6"
+			size="1"
+		/>
+		<button
+			on:click={handleSubmitButtonClick}
+			class="button bg-secondary other-text"
+			type="sumbit"
+			disabled={!(gameid.length === 6)}
+		>
+			Join Game
+		</button>
+	</form>
 </div>
