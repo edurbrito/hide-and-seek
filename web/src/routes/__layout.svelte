dd795f4ac96260597fd969eada47f8e05dce89ed
edurbrito <developer.edurbrito@gmail.com>
2022-04-13 23:24:07 +0300

    add seeker view & map

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -1,5 +1,12 @@
 <script>
-    import "../app.css";
+	import '../app.css';
 </script>
-  
-<slot />
+
+<div
+	id="warning"
+	class="w-screen h-screen bg-primary flex flex-col items-center justify-center text-center text-white p-8 font-bold absolute"
+	style="z-index: 1000000000; display: none;"
+>
+	🙈 Please, turn on Geolocation on your device and refresh this page. 🙉
+</div>
+<slot class="bg-primary" />
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,7 +1,18 @@
+<script lang="ts">
+	async function seek() {
+		await fetch(`http://localhost:5000/seek`, { method: 'POST' })
+			.then((response) => response.json())
+			.then((game) => {
+				localStorage.setItem('game', JSON.stringify(game));
+				window.location.href = `/seek/${game.uuid}/`;
+			});
+	}
+</script>
+
 <div>
 	<h1 class="title">Hide & Seek</h1>
 	<div class="mt-5 flex actions-container">
 		<a href="/hide" class="link bg-secondary other-text" type="button">Hide</a>
-		<button class="button bg-secondary other-text" type="button">Seek</button>
+		<button class="button bg-secondary other-text" type="button" on:click={seek}>Seek</button>
 	</div>
 </div>
